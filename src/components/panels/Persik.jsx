229300d28904c3fde3e806d6976115bb15e14c46
Dima Krutolianov <DimaKrutDK@ya.ru>
2020-10-31 19:34:14 +0200

    refactor: move components to the separate folder

--- src/components/panels/Persik.jsx
+++ src/components/panels/Persik.jsx
@@ -7,7 +7,7 @@ import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back'
 import Icon24Back from '@vkontakte/icons/dist/24/back'
 
-import persik from '../img/persik.png'
+import persik from '../../img/persik.png'
 import './Persik.css'
 
 const osName = platform()
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import 'core-js/features/set'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import bridge from '@vkontakte/vk-bridge'
-import App from './App'
+import App from './components/App'
 import { Utils } from './Utils'
 
 // Init VK  Mini App
