3add2f0ca60c729371e4a1caaa28c44303e0518f
Dima Krutolianov <DimaKrutDK@ya.ru>
2020-10-31 18:58:35 +0200

    chore: allow dynamic import function

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,14 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
     sourceType: 'module',
+    allowImportExportEverywhere: true,
   },
   plugins: ['react', 'react-hooks', 'jest', 'jest-formatting', 'jsx-a11y'],
   rules: {
