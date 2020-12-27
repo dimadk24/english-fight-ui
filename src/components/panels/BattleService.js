73b4d9076bf3bbb0b77e32081df9b0df9d2fdf37
Dima Krutolianov <DimaKrutDK@ya.ru>
2020-12-27 21:43:44 +0300

    refactor: encapsulate model parsing into ApiService

--- src/components/panels/BattleService.js
+++ src/components/panels/BattleService.js
@@ -4,19 +4,20 @@ import { Question } from '../../models/Question'
 
 export class BattleService {
   static async startBattle() {
-    const response = await ApiService.post('game', {}, { expand: 'questions' })
-    return Game.fromObject(response)
+    return ApiService.post('game', {}, { expand: 'questions', Model: Game })
   }
 
   static async submitQuestion(question) {
-    const response = await ApiService.patch(`question/${question.id}`, {
-      selectedAnswer: question.selectedAnswer,
-    })
-    return Question.fromObject(response)
+    return ApiService.patch(
+      `question/${question.id}`,
+      {
+        selectedAnswer: question.selectedAnswer,
+      },
+      { Model: Question }
+    )
   }
 
   static async getBattle(id) {
-    const result = await ApiService.get(`game/${id}`, { expand: 'questions' })
-    return Game.fromObject(result)
+    return ApiService.get(`game/${id}`, { expand: 'questions', Model: Game })
   }
 }
