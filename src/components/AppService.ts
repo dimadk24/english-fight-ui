63ead88ed30177931c3048a746afa534dc3e1fe8
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-23 21:45:48 +0300

    feat: don't request notifications from VK, if already allowed
    In such case only need to change notification status on our api

--- src/components/AppService.ts
+++ src/components/AppService.ts
@@ -4,6 +4,10 @@ import { NOTIFICATIONS_STATUSES } from '../constants'
 import bridge from '@vkontakte/vk-bridge'
 
 export class AppService {
+  static areNotificationsEnabledOnVkSide = Boolean(
+    parseInt(Utils.getSearchParam('vk_are_notifications_enabled'), 10)
+  )
+
   static async fetchUserData(): Promise<UserInstance> {
     return ApiService.get<UserInstance>('user', { Model: User })
   }
@@ -25,6 +29,10 @@ export class AppService {
   }
 
   static async requestNotifications(): Promise<UserInstance> {
+    if (AppService.areNotificationsEnabledOnVkSide) {
+      // already allowed in VK, need only to allow on our backend
+      return AppService.allowNotifications()
+    }
     try {
       const { result } = await bridge.send('VKWebAppAllowNotifications')
       if (result) return AppService.allowNotifications()
