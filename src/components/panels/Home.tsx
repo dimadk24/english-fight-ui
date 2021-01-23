d1f809be7666681eef617ef6c4cc2d521f766d84
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-23 21:23:22 +0300

    feat: manage notifications on the home panel

--- src/components/panels/Home.tsx
+++ src/components/panels/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import Button from '@vkontakte/vkui/dist/components/Button/Button'
 import Group from '@vkontakte/vkui/dist/components/Group/Group'
@@ -6,39 +6,82 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell'
 import Div from '@vkontakte/vkui/dist/components/Div/Div'
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar'
 import PanelHeader from '../helpers/PanelHeader'
+import { Switch } from '@vkontakte/vkui'
+import { NOTIFICATIONS_STATUSES } from '../../constants'
+import { AppService } from '../AppService'
+import { UserInstance } from '../../core/user-model'
+import Loader from '../helpers/Loader'
 
 const Home = ({
   onStartBattle,
   user,
-}: InferProps<typeof Home.propTypes>): JSX.Element => (
-  <>
-    <PanelHeader text="English Clash" showBackButton={false} />
-    {user && (
-      <Group>
-        <Cell
-          before={user.photoUrl ? <Avatar src={user.photoUrl} /> : null}
-          description={
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <span>Количество очков - {user.score}</span>{' '}
-              <span>Место в рейтинге: {user.foreverRank}</span>
-            </div>
-          }
-          multiline
-        >
-          {`${user.firstName} ${user.lastName}`}
-        </Cell>
-      </Group>
-    )}
+  onUpdateUser,
+}: InferProps<typeof Home.propTypes>): JSX.Element => {
+  const [loading, setLoading] = useState(false)
+  const onSwitchNotifications = async (event) => {
+    const { checked: newChecked } = event.target
+    setLoading(true)
+    let updatedUser: UserInstance
+    if (newChecked) {
+      // need to enable
+      updatedUser = await AppService.requestNotifications()
+    } else {
+      // need to disable
+      updatedUser = await AppService.blockNotifications()
+    }
+    setLoading(false)
+    onUpdateUser(updatedUser)
+  }
+  return (
+    <>
+      <PanelHeader text="English Clash" showBackButton={false} />
+      {user && (
+        <Group>
+          <Cell
+            before={user.photoUrl ? <Avatar src={user.photoUrl} /> : null}
+            description={
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <span>Количество очков - {user.score}</span>{' '}
+                <span>Место в рейтинге: {user.foreverRank}</span>
+              </div>
+            }
+            multiline
+          >
+            {`${user.firstName} ${user.lastName}`}
+          </Cell>
+        </Group>
+      )}
 
-    <Group>
-      <Div>
-        <Button size="xl" onClick={onStartBattle}>
-          Начать!
-        </Button>
-      </Div>
-    </Group>
-  </>
-)
+      <Group>
+        <Div>
+          <Button size="xl" onClick={onStartBattle} disabled={loading}>
+            Начать!
+          </Button>
+        </Div>
+      </Group>
+
+      {user && (
+        <Group>
+          <Cell
+            multiline
+            indicator={
+              <Switch
+                checked={
+                  user.notificationsStatus === NOTIFICATIONS_STATUSES.ALLOW
+                }
+                onChange={onSwitchNotifications}
+                disabled={loading}
+              />
+            }
+          >
+            Уведомления об обновлениях
+          </Cell>
+        </Group>
+      )}
+      {loading && <Loader />}
+    </>
+  )
+}
 
 Home.propTypes = {
   onStartBattle: PropTypes.func.isRequired,
@@ -51,6 +94,7 @@ Home.propTypes = {
     monthlyRank: PropTypes.number.isRequired,
     notificationsStatus: PropTypes.string.isRequired,
   }),
+  onUpdateUser: PropTypes.func.isRequired,
 }
 
 Home.defaultProps = {
