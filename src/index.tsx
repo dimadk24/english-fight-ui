c7bc8e10d1d3578faa66f94354fd7ce025a0a2b5
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-04-11 20:59:49 +0300

    fix: not setting theme right after open app

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,9 @@ import 'core-js/features/map'
 import 'core-js/features/set'
 import React from 'react'
 import { render as renderReact } from 'react-dom'
-import bridge from '@vkontakte/vk-bridge'
 import { Utils } from './Utils'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import AppWrapper from './components/AppWrapper'
-
-// Init VK  Mini App
-bridge.send('VKWebAppInit')
 
 function render() {
   renderReact(
